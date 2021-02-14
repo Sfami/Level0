46edfa4874f646c2799eb8daf7a7d85f6a822d53
takalani <tomufamadi@gmail.com>
2021-02-14 20:41:15 +0200

    make requested changes to Task 0.7

--- src/Task0_7.java
+++ src/Task0_7.java
@@ -3,15 +3,18 @@
 public class Task0_7 {
 
     public static void main(String[] args) {
-        System.out.println(celsiusToFahrenheit(32) + " fahrenheit");
-        System.out.println(fahrenheitToCelsius(32) + " celsius");
+        System.out.println(celsiusToFahrenheit(32.00  ) + " fahrenheit");
+        System.out.println(fahrenheitToCelsius(32.00) + " celsius");
     }
     public static double celsiusToFahrenheit(double celsiusTemperature){
-        double fahrenheitTemperature = 9/5 * (celsiusTemperature) + 32;
+        //use double for input instead of integer
+        double fahrenheitTemperature = 9.0/5.0 * (celsiusTemperature) + 32.0;
         return fahrenheitTemperature;
     }
     public static double fahrenheitToCelsius(double fahrenheitTemperature){
-        double celsiusTemperature = 5/9 * (fahrenheitTemperature - 32);
+        //use double for input instead of integer
+        double celsiusTemperature = 5.0/9.0 * (fahrenheitTemperature - 32.0);
         return celsiusTemperature;
     }
+    
 }
