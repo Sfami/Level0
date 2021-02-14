632a1a3fa0fb23bb98297c6895650f7d6f56dad5
takalani <tomufamadi@gmail.com>
2021-02-14 20:41:35 +0200

    make requested changes to Task 0.8

--- src/Task0_8.java
+++ src/Task0_8.java
@@ -2,7 +2,7 @@ public class Task0_8 {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(convertToHours(1));
+        System.out.println(convertToHours(61));
 
     }
     public static String convertToHours(double number){
@@ -17,7 +17,7 @@ public class Task0_8 {
         }
 
         if (Math.floor(number / 60) == 0){
-            hoursString = hoursString;
+            hoursString += Math.floor(number / 60) + " hours";
         }
 
         if (number % 60 > 1){
@@ -25,11 +25,11 @@ public class Task0_8 {
         }
 
         if (number % 60 == 1){
-            hoursString += ", " + number % 60 + "minute ";
+            hoursString += ", " + number % 60 + " minute ";
         }
 
         if (number % 60 == 0){
-            hoursString = hoursString;
+            hoursString += ", " + number % 60 + " minutes ";
         }
 
 
