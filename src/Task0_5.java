e17d4552f838f2d9ff230961549cc4a11d76dcb8
Sfami <tomufamadi@gmail.com>
2021-07-02 21:01:55 +0200

    made changes to areaOfTriangle function i.e renamed s to semiPerimeter

--- src/Task0_5.java
+++ src/Task0_5.java
@@ -1,12 +1,12 @@
 public class Task0_5 {
 
     public static void main(String[] args) {
-	    System.out.println(areaOfTriangle(4,5,3));
+        System.out.println(areaOfTriangle(4,5,3));
     }
 
     public static double areaOfTriangle(int side1,int side2,int side3){
-        double s = 0.5 * (side1 + side2 + side3);//finding semi-perimeter of the triangle
-        double area = Math.sqrt(s * (s - side1) * (s -side2) * (s - side3));
+        double semiPerimeter = 0.5 * (side1 + side2 + side3);
+        double area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter -side2) * (semiPerimeter - side3));
         return area;
     }
 }
